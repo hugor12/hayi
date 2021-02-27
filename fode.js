5bcc66914fc4e47542e433bf0dc7499bb9d3942f
hugor12 <79744227+hugor12@users.noreply.github.com>
2021-02-27 11:36:12 +0200

    Add files via upload

---
+++ fode.js
@@ -0,0 +1,94 @@
+$(document).ready(function(){
+
+
+// $(".hide").click(function(){
+// $("p").text("bhoapromvj9bn;kmpmpnp,bng;[ nhrne8y,9v,");});
+  
+
+//alert($("p").text());
+//});
+
+//$(".show").click(function(){
+//$("p").slideDown(10000);
+//});
+ 
+//$(".hide").mouseenter(function(){
+
+
+//$("P").hide();
+//});
+//$(".show").mouseenter(function(){
+//$("p").show();
+//});
+//$(".hide").mouseleave(function(){
+ // $("p").hide();
+//})
+//$(".show").click(function(){
+  //$("p").toggle();
+//});
+//$(".hide").dblclick(function(){
+//$("p").toggle();
+//});
+//$(".hide").hover(function(){
+//$("P").hide();
+//},function(){
+//$("p").show();
+//});
+
+// $("#gogo").click(function(){
+// var tulo=$("#fewa").val(242);
+// var gebe=$("#buter").val(234);
+// var here= parseInt(tulo);
+// var dodo= parseInt(gebe);
+// var result=here+dodo;
+// alert(result);
+// });
+//$("p").hover (function(){
+  //$(this).toggleClass("gere");
+//});
+  $("p").click (function(){
+  $(this).removeClass("peter");
+  
+
+  });
+  $("li").hover(function(){
+    $(this).toggleClass("fode");
+  });
+  $(".tire").hover(function(){
+    $(".rentre").toggle();
+  })
+  $(".tire2").hover(function(){
+    $(".rentre2").toggle();
+  })
+  $(".tire3").hover(function(){
+    $(".rentre3").toggle();
+  })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
